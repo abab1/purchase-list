8068c8981158bde4c71ad151b40397f8fc3aa8ae
abab1 <abab1@umbc.edu>
2025-01-20 00:56:06 -0800

    size fx

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/purchase-list/"
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './tests/setup.ts'
-  }
+  },
 });
